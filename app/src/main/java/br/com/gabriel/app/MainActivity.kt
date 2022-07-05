77c023f5b9f511b5e8aeeec2a03dc12b3f077ce5
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2022-07-05 17:01:24 -0300

    Resolving rows for numbers

--- app/src/main/java/br/com/gabriel/app/MainActivity.kt
+++ app/src/main/java/br/com/gabriel/app/MainActivity.kt
@@ -16,36 +16,62 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxHeight(0.2f)
-                    .fillMaxWidth()
-                    .background(Color(0xff7159c1)),
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxSize()
             ) {
-                Text(
-                    text = "Result",
-                    color = Color.White,
-                    fontSize = 32.sp,
+                Row(
                     modifier = Modifier
-                        .padding(24.dp)
-                )
-            }
-            Row {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
-            Row {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            Row {
-                Text("1")
-                Text("2")
-                Text("3")
+                        .fillMaxHeight(0.2f)
+                        .fillMaxWidth()
+                        .background(Color(0xff7159c1)),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Result",
+                        color = Color.White,
+                        fontSize = 32.sp,
+                        modifier = Modifier
+                            .padding(24.dp)
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .height(150.dp)
+                        .fillMaxWidth()
+                        .background(Color.Blue),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("7")
+                    Text("8")
+                    Text("9")
+                }
+                Row(
+                    modifier = Modifier
+                        .height(150.dp)
+                        .fillMaxWidth()
+                        .background(Color.Cyan),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                }
+                Row(
+                    modifier = Modifier
+                        .height(150.dp)
+                        .fillMaxWidth()
+                        .background(Color.Red),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
             }
         }
     }
