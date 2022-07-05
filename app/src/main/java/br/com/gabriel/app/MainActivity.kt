90a2f723634128367f526ab71d572d0c11f4a21c
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2022-07-05 17:43:33 -0300

    Adjusting rows for columns for numbers

--- app/src/main/java/br/com/gabriel/app/MainActivity.kt
+++ app/src/main/java/br/com/gabriel/app/MainActivity.kt
@@ -3,12 +3,19 @@ package br.com.gabriel.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -33,44 +40,203 @@ class MainActivity : ComponentActivity() {
                         color = Color.White,
                         fontSize = 32.sp,
                         modifier = Modifier
-                            .padding(24.dp)
+                            .padding(40.dp)
                     )
                 }
                 Row(
                     modifier = Modifier
-                        .height(150.dp)
                         .fillMaxWidth()
-                        .background(Color.Blue),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
+                        .background(Color.DarkGray),
+                    horizontalArrangement = Arrangement.SpaceAround
                 ) {
-                    Text("7")
-                    Text("8")
-                    Text("9")
-                }
-                Row(
-                    modifier = Modifier
-                        .height(150.dp)
-                        .fillMaxWidth()
-                        .background(Color.Cyan),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text("4")
-                    Text("5")
-                    Text("6")
-                }
-                Row(
-                    modifier = Modifier
-                        .height(150.dp)
-                        .fillMaxWidth()
-                        .background(Color.Red),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text("1")
-                    Text("2")
-                    Text("3")
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight(),
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Card {
+                            Text(
+                                text = "C",
+                                 modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+
+                        Card {
+                            Text(
+                                text = "7",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "4",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "1",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight(),
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Card {
+                            Text(
+                                text = "<",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "8",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "5",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "2",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight(),
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Card {
+                            Text(
+                                text = "/",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "9",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "6",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "6",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight(),
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Card {
+                            Text(
+                                text = "*",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "-",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "+",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Card {
+                            Text(
+                                text = "=",
+                                modifier = Modifier
+                                    .background(Color.LightGray)
+                                    .padding(40.dp),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
                 }
             }
         }
